a2ad54b1e392be8a1e00597b770f0efb3d8d68bd
Behnam <behnam.ebrahimi@gmail.com>
2020-01-14 18:57:33 +1100

    Update ResolveAuthScreen.js

--- track/src/screens/ResolveAuthScreen.js
+++ track/src/screens/ResolveAuthScreen.js
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { tryLocalSignin } from '../actions/authActions';
 
-const FacebookLoginScreen = ({ tryLocalSignin }) => {
+const ResolveAuthScreen = ({ tryLocalSignin }) => {
   useEffect(() => {
     tryLocalSignin();
   }, []);
   return null;
 };
 
-export default connect(null, { tryLocalSignin })(FacebookLoginScreen);
+export default connect(null, { tryLocalSignin })(ResolveAuthScreen);
